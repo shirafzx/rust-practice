f1dd525259ce6fb46854fa4f3f3e05a559eedae8
Shirafzx <shirafzx@gmail.com>
2024-12-02 23:28:57 +0700

    17 smart pointer and 18 traits as a type

--- 17-smart-pointers/src/main.rs
+++ 17-smart-pointers/src/main.rs
@@ -1,3 +1,17 @@
+use std::{cell::RefCell, rc::Rc};
+
 fn main() {
-    println!("Hello, world!");
+    // let chest: Box<i32> = Box::new(10);
+    // let shared_chest: Rc<RefCell<Box<i32>>> = Rc::new(RefCell::new(chest));
+
+    // **shared_chest.borrow_mut() += 10;
+    // **shared_chest.borrow_mut() += 5;
+
+    let chest: i32 = 10;
+    let shared_chest: Rc<RefCell<i32>> = Rc::new(RefCell::new(chest));
+    
+    *shared_chest.borrow_mut() += 10;
+    *shared_chest.borrow_mut() += 5;
+
+    println!("Gold: {}", shared_chest.borrow());
 }
--- 18-traits-as-a-type/src/main.rs
+++ 18-traits-as-a-type/src/main.rs
@@ -1,3 +1,51 @@
+trait Gear {
+    fn use_gear(&self);
+}
+
+struct Sword;
+struct Bow;
+struct Potion;
+
+// fn use_gear<T: Gear>(item: T) {
+//     item.use_gear();
+// }
+
+// fn use_gear(item: Box<dyn Gear>) {
+//     item.use_gear();
+// }
+
+fn use_gear(item: &impl Gear) {
+    item.use_gear();
+}
+
+impl Gear for Sword {
+    fn use_gear(&self) {
+        println!("Swing sword");
+    }
+}
+
+impl Gear for Bow {
+    fn use_gear(&self) {
+        println!("Fire arrow");
+    }
+}
+
+impl Gear for Potion {
+    fn use_gear(&self) {
+        println!("Drink potion");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let crabby_sword: Sword = Sword;
+    let crabby_bow: Bow = Bow;
+    let crabby_potion: Potion = Potion;
+
+    // let crabby_sword: Box<Sword> = Box::new(Sword);
+    // let crabby_bow: Box<Bow> = Box::new(Bow);
+    // let crabby_potion: Box<Potion> = Box::new(Potion);
+
+    use_gear(&crabby_sword);
+    use_gear(&crabby_bow);
+    use_gear(&crabby_potion);
 }
